bd468b51c3612a4c23859c11f38e4721fff6b8b1
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-24 20:33:23 -0400

    Show episode duration on player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -5,6 +5,7 @@ import 'rc-slider/assets/index.css';
 
 import styles from './styles.module.scss';
 import { usePlayer } from '../../contexts/PlayerContext';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
@@ -72,7 +73,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             )}
           </div>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
         {episode && (
